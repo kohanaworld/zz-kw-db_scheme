9e58dff1e0a2c21ea444bac850f3644fa7f9a2ed
smgladkovskiy <SMGladkovskiy@gmail.com>
2011-02-14 10:59:13 +0300

    table comments and crawlers, crawler_stats and logs tables

--- sql_schema.sql
+++ sql_schema.sql
@@ -10,15 +10,15 @@ USE `russiankohana_kohanaworld_dev` ;
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`developers` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `github_id` INT NOT NULL COMMENT 'идентификатор разработчика на github' ,
-  `username` VARCHAR(128) NOT NULL COMMENT 'nickname разработчика' ,
-  `date_create` INT UNSIGNED NOT NULL COMMENT 'дата создания аккаунта на github' ,
-  `url` VARCHAR(256) NOT NULL COMMENT 'url страницы разработчика на Github' ,
-  `realname` VARCHAR(256) NULL DEFAULT NULL COMMENT 'Полное имя разработчика' ,
-  `email` VARCHAR(256) NULL DEFAULT NULL COMMENT 'email разработчика' ,
-  `location` VARCHAR(256) NULL DEFAULT NULL COMMENT 'где живёт' ,
-  `company` VARCHAR(256) NULL DEFAULT NULL COMMENT 'название компании' ,
-  `blog_url` VARCHAR(256) NULL DEFAULT NULL COMMENT 'URL блога/сайта' ,
+  `github_id` INT NOT NULL COMMENT 'GitHub developer ID' ,
+  `username` VARCHAR(128) NOT NULL COMMENT 'developer nickname' ,
+  `date_create` INT UNSIGNED NOT NULL COMMENT 'GitHub account creation date' ,
+  `url` VARCHAR(256) NOT NULL COMMENT 'Github developer URL' ,
+  `realname` VARCHAR(256) NULL DEFAULT NULL COMMENT 'developer Fullname' ,
+  `email` VARCHAR(256) NULL DEFAULT NULL COMMENT 'developer email' ,
+  `location` VARCHAR(256) NULL DEFAULT NULL COMMENT 'developer location' ,
+  `company` VARCHAR(256) NULL DEFAULT NULL COMMENT 'developer company' ,
+  `blog_url` VARCHAR(256) NULL DEFAULT NULL COMMENT 'blog/site URL' ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB
 COMMENT = 'Таблица с данными о разработчиках';
@@ -29,17 +29,17 @@ COMMENT = 'Таблица с данными о разработчиках';
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`modules` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'идентификатор модуля' ,
-  `developer_id` INT UNSIGNED NOT NULL COMMENT 'автор (владелец) модуля' ,
-  `name` VARCHAR(128) NOT NULL COMMENT 'название модуля' ,
-  `fullname` VARCHAR(255) NOT NULL COMMENT 'полное название модуля (:username/:name)' ,
-  `url` VARCHAR(255) NOT NULL COMMENT 'URL модуля на Github\'е' ,
-  `homepage` VARCHAR(255) NULL DEFAULT NULL COMMENT 'домашняя страница модуля' ,
-  `description` TEXT NOT NULL COMMENT 'описание модуля' ,
-  `date_create` INT NOT NULL ,
-  `date_update` INT NULL DEFAULT NULL ,
-  `has_wiki` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'заведена wiki' ,
-  `has_issues` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'имеются открытые тикеты' ,
-  `has_downloads` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'имеются файлы для скачивания *' ,
+  `developer_id` INT UNSIGNED NOT NULL COMMENT 'module developer (author)' ,
+  `name` VARCHAR(128) NOT NULL COMMENT 'module name' ,
+  `fullname` VARCHAR(255) NOT NULL COMMENT 'module fullname (:username/:name)' ,
+  `url` VARCHAR(255) NOT NULL COMMENT 'Github module URL' ,
+  `homepage` VARCHAR(255) NULL DEFAULT NULL COMMENT 'module homepage' ,
+  `description` TEXT NOT NULL COMMENT 'module description' ,
+  `date_create` INT NOT NULL COMMENT 'date of module creation' ,
+  `date_update` INT NULL DEFAULT NULL COMMENT 'module last update date' ,
+  `has_wiki` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'wiki availability' ,
+  `has_issues` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'open issues availability' ,
+  `has_downloads` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'is there are files to downlod' ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `fullname_UNIQUE` (`fullname` ASC) ,
   INDEX `fk_module_developer` (`developer_id` ASC) ,
@@ -49,7 +49,7 @@ CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`modules` (
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB
-COMMENT = 'Таблица для хранения информации о модулях';
+COMMENT = 'Modules main info';
 
 
 -- -----------------------------------------------------
@@ -57,13 +57,14 @@ COMMENT = 'Таблица для хранения информации о мод
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`module_info` (
   `id` INT NOT NULL ,
-  `forks` SMALLINT NOT NULL COMMENT 'количество форков' ,
-  `watchers` SMALLINT NOT NULL COMMENT 'количество наблюдателей' ,
-  `tags` SMALLINT NOT NULL COMMENT 'количество тэгов' ,
-  `score` FLOAT(4,3) NOT NULL COMMENT 'Оценка Github' ,
-  `issues_opened` SMALLINT UNSIGNED NOT NULL DEFAULT 0 ,
-  `issues_closed` SMALLINT UNSIGNED NOT NULL DEFAULT 0 ,
-  `module_id` INT UNSIGNED NOT NULL ,
+  `module_id` INT UNSIGNED NOT NULL COMMENT 'module id' ,
+  `forks` SMALLINT NOT NULL COMMENT 'number of module forks' ,
+  `watchers` SMALLINT NOT NULL COMMENT 'number of module watchers' ,
+  `tags` SMALLINT NOT NULL COMMENT 'number of module tags' ,
+  `score` FLOAT(4,3) NOT NULL COMMENT 'Github module score' ,
+  `issues_opened` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of opened issueses' ,
+  `issues_closed` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of closed issueses' ,
+  `date_update` INT UNSIGNED NOT NULL COMMENT 'module update date' ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_module-info_module` (`module_id` ASC) ,
   CONSTRAINT `fk_module-info_module`
@@ -72,7 +73,7 @@ CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`module_info` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
-COMMENT = 'Таблица для хранения статистики по модулям';
+COMMENT = 'Modules information collection';
 
 
 -- -----------------------------------------------------
@@ -80,10 +81,10 @@ COMMENT = 'Таблица для хранения статистики по мо
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`modules_dependences` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID зависимости' ,
-  `subject_id` INT UNSIGNED NOT NULL COMMENT 'зависящий модуль' ,
-  `target_id` INT UNSIGNED NOT NULL COMMENT 'от кого зависит' ,
-  `original_target_link` VARCHAR(256) NULL DEFAULT NULL COMMENT 'URL, прописанный в .gitmodules' ,
-  `path` VARCHAR(128) NULL DEFAULT NULL COMMENT 'Относительный путь в модуле, где лежит зависимость' ,
+  `subject_id` INT UNSIGNED NOT NULL COMMENT 'depended module' ,
+  `target_id` INT UNSIGNED NOT NULL COMMENT 'tardet module' ,
+  `original_target_link` VARCHAR(256) NULL DEFAULT NULL COMMENT 'dependence URL (in .gitmodules)' ,
+  `path` VARCHAR(128) NULL DEFAULT NULL COMMENT 'relative path in module where dependence is located' ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_module-dependance_subject` (`subject_id` ASC) ,
   INDEX `fk_module-dependance_target` (`target_id` ASC) ,
@@ -98,7 +99,7 @@ CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`modules_dependences
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
-COMMENT = 'Таблица для хранения зависимостей между модулями';
+COMMENT = 'modules dependences';
 
 
 -- -----------------------------------------------------
@@ -124,9 +125,9 @@ COMMENT = 'Пользователи ресурса';
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`developer_info` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `followers` SMALLINT UNSIGNED NOT NULL DEFAULT 0 ,
-  `own_repos` SMALLINT UNSIGNED NOT NULL DEFAULT 0 ,
-  `developer_id` INT UNSIGNED NOT NULL ,
+  `developer_id` INT UNSIGNED NOT NULL COMMENT 'developer ID' ,
+  `followers` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of followers' ,
+  `own_repos` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of own repos' ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_developer_info-developer` (`developer_id` ASC) ,
   CONSTRAINT `fk_developer_info-developer`
@@ -143,8 +144,8 @@ COMMENT = 'Дополнительная информация о разработ
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`modules_contributors` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `module_id` INT UNSIGNED NOT NULL COMMENT 'id модуля' ,
-  `developer_id` INT UNSIGNED NOT NULL COMMENT 'id разработчика' ,
+  `module_id` INT UNSIGNED NOT NULL COMMENT 'module ID' ,
+  `developer_id` INT UNSIGNED NOT NULL COMMENT 'developer ID' ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_module-contributor_module` (`module_id` ASC) ,
   INDEX `fk_module-contributor_contributor` (`developer_id` ASC) ,
@@ -182,11 +183,12 @@ COMMENT = 'Application logs';
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`crawler_stats` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `log_id` INT UNSIGNED NOT NULL ,
+  `log_id` INT UNSIGNED NOT NULL COMMENT 'log record ID' ,
   `new_modules` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of new modules' ,
   `updated_modules` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of updated modules' ,
   `new_developers` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of new developers' ,
   `updated_developers` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of updated developers' ,
+  `new_tags` SMALLINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'number of new github tags' ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_crawler-stat_log` (`log_id` ASC) ,
   CONSTRAINT `fk_crawler-stat_log`
@@ -196,6 +198,55 @@ CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`crawler_stats` (
     ON UPDATE CASCADE)
 ENGINE = InnoDB
 COMMENT = 'Github crowles job statuses';
+
+
+-- -----------------------------------------------------
+-- Table `russiankohana_kohanaworld_dev`.`module_tags`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`module_tags` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(256) NOT NULL COMMENT 'tag name' ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+COMMENT = 'Module tags in thoughts of GitHub';
+
+
+-- -----------------------------------------------------
+-- Table `russiankohana_kohanaworld_dev`.`modules_module_tags`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`modules_module_tags` (
+  `module_id` INT UNSIGNED NOT NULL ,
+  `module_tag_id` INT UNSIGNED NOT NULL ,
+  PRIMARY KEY (`module_id`, `module_tag_id`) ,
+  INDEX `fk_module_module-tag` (`module_tag_id` ASC) ,
+  INDEX `fk_module-tag_module` (`module_id` ASC) ,
+  CONSTRAINT `fk_module_module-tag`
+    FOREIGN KEY (`module_tag_id` )
+    REFERENCES `russiankohana_kohanaworld_dev`.`module_tags` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_module-tag_module`
+    FOREIGN KEY (`module_id` )
+    REFERENCES `russiankohana_kohanaworld_dev`.`modules` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `russiankohana_kohanaworld_dev`.`crawlers`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `russiankohana_kohanaworld_dev`.`crawlers` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `type` VARCHAR(100) NOT NULL ,
+  `page` SMALLINT(5) UNSIGNED NOT NULL ,
+  `updated` INT(10) UNSIGNED NOT NULL ,
+  `stopped` TINYINT(1) NOT NULL DEFAULT '0' ,
+  PRIMARY KEY (`id`) ,
+  INDEX `unique_type` (`type` ASC) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
 
 
 
